3c994305a81bff3c32d88cfe0139334edcd59bf1
naneps <ekaprasetya2244@gmail.com>
2022-06-28 23:52:54 +0700

    create mainjs and index

---
+++ intro-to-Vue3/main.js
@@ -0,0 +1,13 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            product : {
+                name : "Kaos Kaki",
+                price : 1000000,
+                description :'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor',
+                image:'./assets/images/socks_blue.jpg'
+            }
+           
+        }
+    }
+})
