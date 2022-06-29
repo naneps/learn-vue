daaa3ae8a58be1feab6c3b846f19149bac0837e2
naneps <ekaprasetya2244@gmail.com>
2022-06-29 10:16:33 +0700

    adding product

--- intro-to-Vue3/main.js
+++ intro-to-Vue3/main.js
@@ -1,13 +1,35 @@
 const app = Vue.createApp({
     data(){
         return{
+            dataProducts: [
+
+            ],
+            cart:0,
             product : {
                 name : "Kaos Kaki",
                 price : 1000000,
                 description :'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor',
-                image:'./assets/images/socks_blue.jpg'
+                image:'./assets/images/socks_blue.jpg',
+                url : 'https://getbootstrap.com/docs/5.0/components/card/',
+                inStock: false,
+                inventory:0,
+                onSale:false,
+                details: [
+                    "50% Catoon " ,"30 Wool", "20% Polyster"
+                ],
+                variants:[
+                    { id : 2343 , color: 'green'},
+                    { id : 2654 , color: 'blue'}
+                ],
+                size: [
+                    32 , 25 , 20
+                ]
+                
             }
            
         }
-    }
+    },
+    methods: {
+         
+    },
 })
